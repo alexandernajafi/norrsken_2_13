2aadc45cd0fe46171e3952b6e794a4f682479c8b
Tomas Nilsson <tomas.nilsson92@gmail.com>
2019-09-17 15:01:22 +0200

    Setup reducer logic

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,12 @@ import React from 'react'
 import HeatMap from './components/HeatMap'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <HeatMap
-        data={[
-          { lat: 59.33258, lng: 18.0649 },
-          { lat: 58.33258, lng: 18.0649 },
-        ]}
-      />
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <HeatMap
+      data={[{ lat: 59.33258, lng: 18.0649 }, { lat: 58.33258, lng: 18.0649 }]}
+    />
+  </div>
+)
 
 export default App
