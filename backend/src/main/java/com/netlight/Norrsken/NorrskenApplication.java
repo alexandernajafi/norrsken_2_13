3822540dd3987349d5981ca42b8b91eaf208f1ed
Alexander Najafi <alexander.najafi@netlight.com>
2019-09-17 13:06:25 +0200

    Init commit

---
+++ backend/src/main/java/com/netlight/Norrsken/NorrskenApplication.java
@@ -0,0 +1,13 @@
+package com.netlight.Norrsken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorrskenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorrskenApplication.class, args);
+	}
+
+}
